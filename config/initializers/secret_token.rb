dc601f04e92535306f50101c1cf46ce4ae5ca4f3
Jonathan Yu <jonathan@jonathanyu.net>
2013-06-29 19:19:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readingapp::Application.config.secret_token = '479ae63591383bcf0772d39121ea18913cd6d1dee9dc0d1b65ae7e6ba830f9c8d8a5b0cbf48c29691de1e654b14399774ee5f1405272c6a8b4d6710e424aedfb'
