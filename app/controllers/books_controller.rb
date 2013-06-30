ed06aa579153c2a5dc18c49aa628d86bfa786054
Jonathan Yu <jonathan@jonathanyu.net>
2013-06-30 12:44:58 -0700

    Added home and explore pages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,12 @@
 class BooksController < ApplicationController
+  
+  def index
+    render :explore
+  end
+  
+  def show
+    @book = Book.find(param[:id])
+    render :show
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Readingapp::Application.routes.draw do
   
+  root :to => 'root#home'
   match 'login', :to => 'session#login'
   match 'signup', :to => 'users#new'
   resource :session, :only => [:create, :destroy]
