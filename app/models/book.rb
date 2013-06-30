25d1eb512cdefd5033fefc6a8da9182436ada5c7
Jonathan Yu <jonathan@jonathanyu.net>
2013-06-30 09:56:59 -0700

    Create controllers and user password

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,3 @@
 class Book < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :author, :synopsis, :cover_image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :email, :password
+  
+  def password=(password)
+    self.password_hash = Digest::SHA2.base64digest(password)
+  end
+  
+  def verify_password(password)
+    self.password_hash == Digest::SHA2.base64digest(password)
+  end
+  
+  def generate_token!
+    self.token = SecureRandom.urlsafe_base64(24)
+    self.save!
+    
+    self.token
+  end
 end
