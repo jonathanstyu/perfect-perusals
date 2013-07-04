231ca74088785eabf795704906ace50da1b07f5d
Jonathan Yu <jonathan@jonathanyu.net>
2013-07-04 10:09:56 -0700

    Adds friending button module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :require_login, except: [:new, :show]
+  
   
   def new
     render :signup
@@ -19,6 +21,26 @@ class UsersController < ApplicationController
   
   def edit
     render :account
+  end
+  
+  def friend    
+    @person_to_friend = User.find(params[:user_id])
+    @current_user.friend(@person_to_friend)
+    @friendship = @current_user
+      .friendships
+      .where(friendee_id: @person_to_friend.id).first
+      
+    # render json: @friendship
+    redirect_to :back
+  end
+  
+  def unfriend
+    @friendship = @current_user
+      .friendships
+      .where(friendee_id: params[:user_id]).first
+    
+    @friendship.destroy()
+    redirect_to :back
   end
 
 end
